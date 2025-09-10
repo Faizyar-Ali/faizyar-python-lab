d44d21657e208fe48d525c212cbb663cd168d4f1
Faizyar Ali <shaikhfaizy84@gmail.com>
2025-09-10 21:00:06 +0500

    feat: add day2 basics (variables,input,list,loop)

---
+++ day2_basics/variables_io.py
@@ -0,0 +1,8 @@
+username=input("Your Name Here : ")
+age=int(input("Whats your Age : "))
+print(f"Name={username.upper()} \nAge = {age}")
+list=[10,3.,12]
+list[1]=11
+print(list)
+for i in range(5):
+       print(i+1)
